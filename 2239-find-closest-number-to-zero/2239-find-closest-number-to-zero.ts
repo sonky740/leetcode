e887e6167a917e94a2337257d4eadb7593c147bf
Sonky740 <mykiller4@naver.com>
2023-01-23 01:28:04 +0900

    Time: 63 ms (100.00%), Space: 47 MB (6.67%) - LeetHub

--- 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.ts
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.ts
@@ -1,23 +1,26 @@
 function findClosestNumber(nums: number[]): number {
-  let answer = 0;
-  const temp: number[] = [];
-  const absNums = [...nums].map((num) => Math.abs(num));
+  const nArr: number[] = [];
+  const pArr: number[] = [];
 
-  for (let i = 0; i < absNums.length; i++) {
-    if (absNums[i] === Math.min(...absNums)) {
-      temp.push(i);
+  for (const ele of nums) {
+    if (ele === 0) {
+      return 0;
+    } else if (ele > 0) {
+      pArr.push(ele);
+    } else {
+      nArr.push(ele);
     }
   }
 
-  if (temp.length === 1) {
-    answer = nums[temp[0]];
+  if (!pArr.length) return Math.max(...nArr);
+  if (!nArr.length) return Math.min(...pArr);
+
+  const pMin = Math.min(...pArr);
+  const nMin = Math.max(...nArr);
+
+  if (pMin > Math.abs(nMin)) {
+    return nMin;
   } else {
-    const temp2: number[] = [];
-    for (let i = 0; i < temp.length; i++) {
-      temp2.push(nums[temp[i]]);
-    }
-    answer = Math.max(...temp2);
+    return pMin;
   }
-
-  return answer;
 }
