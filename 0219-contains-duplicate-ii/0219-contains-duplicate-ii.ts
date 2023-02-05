c9548ed0a5d763ae34e1cf4c646860108f2238f3
Sonky740 <mykiller4@naver.com>
2023-02-05 19:33:49 +0900

    Time: 118 ms (78.43%), Space: 62.8 MB (41.67%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
@@ -1,11 +1,16 @@
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
   let answer = false;
+  const map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] === nums[j] && j - i <= k) {
+    if (map.has(nums[i])) {
+      if (i - map.get(nums[i]) <= k) {
         answer = true;
         break;
+      } else {
+        map.set(nums[i], i);
       }
+    } else {
+      map.set(nums[i], i);
     }
   }
 
