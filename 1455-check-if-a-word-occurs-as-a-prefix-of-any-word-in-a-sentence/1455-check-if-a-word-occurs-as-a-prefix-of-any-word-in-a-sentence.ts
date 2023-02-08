3c65c8fefba4b8d3ba0b99ae6cf59fffd95df4b1
Sonky740 <mykiller4@naver.com>
2023-02-08 16:17:43 +0900

    Time: 57 ms (88.89%), Space: 43.4 MB (5.56%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.ts
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.ts
@@ -1,9 +1,6 @@
 function isPrefixOfWord(sentence: string, searchWord: string): number {
-  const words = sentence.split(' ');
-  for (let i = 0; i < words.length; i++) {
-    if (words[i].startsWith(searchWord)) {
-      return i + 1;
-    }
-  }
-  return -1;
+  const answer = sentence
+    .split(' ')
+    .findIndex((word: string) => word.startsWith(searchWord));
+  return answer === -1 ? answer : answer + 1;
 }
