41ce51a95b97f55efc0799b4b07179373f2c7426
Sonky740 <mykiller4@naver.com>
2023-02-10 23:51:16 +0900

    Time: 78 ms (55.56%), Space: 42.8 MB (77.78%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.ts
+++ 0868-binary-gap/0868-binary-gap.ts
@@ -1,10 +1,8 @@
-function binaryGap(n: number): number {
-  return Math.max(
+const binaryGap = (N: number) =>
+  Math.max(
     0,
-    ...n
-      .toString(2)
+    ...N.toString(2)
       .split('1')
       .slice(1, -1)
       .map((gap) => gap.length + 1)
   );
-}
