95b8a101ada134138c25a1f0b017ffef88f9b393
Sonky740 <mykiller4@naver.com>
2023-03-12 02:40:19 +0900

    Time: 81 ms (37.71%), Space: 44.9 MB (50.82%) - LeetHub

---
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.ts
@@ -0,0 +1,13 @@
+function countKDifference(nums: number[], k: number): number {
+  let answer = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (Math.abs(nums[i] - nums[j]) === k) {
+        answer++;
+      }
+    }
+  }
+
+  return answer;
+}
