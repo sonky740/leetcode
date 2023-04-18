8a1496554b65c9ae0a4d4d9416c4fd7cbf168c39
Sonky740 <mykiller4@naver.com>
2023-04-19 00:05:21 +0900

    Time: 359 ms (79.17%), Space: 78.5 MB (62.50%) - LeetHub

---
+++ 2623-memoize/2623-memoize.ts
@@ -0,0 +1,28 @@
+type Fn = (...params: any) => any;
+
+function memoize(fn: Fn): Fn {
+  const cache: Record<string, any> = {};
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (cache.hasOwnProperty(key)) {
+      return cache[key];
+    }
+
+    const result = fn(...args);
+    cache[key] = result;
+    return result;
+  };
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
