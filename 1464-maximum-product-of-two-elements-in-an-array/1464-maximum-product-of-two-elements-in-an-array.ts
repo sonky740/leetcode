d75f8d22287b36cfe809f0dfbe435e09a63b333a
Sonky740 <mykiller4@naver.com>
2023-04-24 00:04:41 +0900

    Time: 114 ms (5.00%), Space: 64.5 MB (6.67%) - LeetHub

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.ts
@@ -0,0 +1,12 @@
+function maxProduct(nums: number[]): number {
+  let answer = 0;
+  const temp: number[] = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      temp.push((nums[i] - 1) * (nums[j] - 1));
+    }
+  }
+
+  return (answer = Math.max(...temp));
+}
