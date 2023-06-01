5911741b8e2944398a018c5171d163783ebc5c1a
Sonky740 <mykiller4@naver.com>
2023-06-02 00:23:28 +0900

    Time: 105 ms (26.11%), Space: 45.3 MB (14.75%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.ts
+++ 0069-sqrtx/0069-sqrtx.ts
@@ -1,3 +1,9 @@
 function mySqrt(x: number): number {
-  return Math.floor(Math.sqrt(x));
-};
+  // return Math.floor(Math.sqrt(x));
+
+  for (let i = 0; i <= x + 1; i++) {
+    if (i * i > x) return i - 1;
+  }
+
+  return 0;
+}
