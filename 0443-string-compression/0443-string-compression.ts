884cd67a254f5a777566f22cb35ac1ef98fc754b
Sonky740 <mykiller4@naver.com>
2023-06-22 00:19:40 +0900

    Time: 86 ms (13.11%), Space: 45 MB (89.75%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.ts
@@ -0,0 +1,24 @@
+function compress(chars: string[]): number {
+  let index = 0;
+  let i = 0;
+
+  while (i < chars.length) {
+    let j = i;
+
+    while (j < chars.length && chars[j] === chars[i]) {
+      j++;
+    }
+    chars[index++] = chars[i];
+
+    if (j - i > 1) {
+      let count = (j - i).toString();
+      for (let k = 0; k < count.length; k++) {
+        chars[index++] = count[k];
+      }
+    }
+
+    i = j;
+  }
+
+  return index;
+}
