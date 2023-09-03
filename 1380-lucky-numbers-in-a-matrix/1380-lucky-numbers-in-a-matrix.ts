c99d0ec5d31366a2b761bf892c42f59ba50c90af
Sonky740 <mykiller4@naver.com>
2023-09-04 00:33:54 +0900

    Time: 59 ms (78.38%), Space: 45.6 MB (51.35%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.ts
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.ts
@@ -1,27 +1,10 @@
-function luckyNumbers (matrix: number[][]): number[] {
-  const answer: number[] = [];
-  const min: number[] = [];
-  const max: number[] = [];
-
+function luckyNumbers(matrix: number[][]): number[] {
   for (let i = 0; i < matrix.length; i++) {
-    min.push(Math.min(...matrix[i]));
+    let min = Math.min(...matrix[i]);
+    const idx = matrix[i].indexOf(min);
+
+    if (matrix.every((arr) => arr[idx] <= min)) return [min];
   }
 
-  for (let i = 0; i < matrix[0].length; i++) {
-    let temp = 0;
-    for (let j = 0; j < matrix.length; j++) {
-      if (matrix[j][i] > temp) {
-        temp = matrix[j][i];
-      }
-    }
-    max.push(temp);
-  }
-
-  for (let i = 0; i < min.length; i++) {
-    if (max.includes(min[i])) {
-      answer.push(min[i]);
-    }
-  }
-
-  return answer;
-};
+  return [];
+}
