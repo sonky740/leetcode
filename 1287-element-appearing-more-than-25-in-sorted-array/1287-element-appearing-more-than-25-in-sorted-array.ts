6b42f3ffc5dc7aba2465afefb0f82f5455e6ea7c
Sonky740 <mykiller4@naver.com>
2023-10-07 00:29:09 +0900

    Time: 48 ms (96.08%), Space: 44.4 MB (92.16%) - LeetHub

---
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.ts
@@ -0,0 +1,12 @@
+function findSpecialInteger(arr: number[]): number {
+    if (arr.length === 1) return arr[0];
+        let count = 1;
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] === arr[i-1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+        if (count > arr.length/4) return arr[i];
+    }
+};
