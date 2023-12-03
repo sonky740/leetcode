0611be1a72f02f84f7437227b289fe31b9f2ba3f
Sonky740 <mykiller4@naver.com>
2023-12-04 00:32:16 +0900

    Time: 68 ms (26.47%), Space: 44.6 MB (55.88%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.ts
@@ -0,0 +1,11 @@
+function minTimeToVisitAllPoints(points: number[][]): number {
+  let answer = 0;
+  for (let i = 0; i < points.length - 1; i++) {
+    const [x1, y1] = points[i];
+    const [x2, y2] = points[i + 1];
+    const xDiff = Math.abs(x2 - x1);
+    const yDiff = Math.abs(y2 - y1);
+    answer += Math.max(xDiff, yDiff);
+  }
+  return answer;
+};
