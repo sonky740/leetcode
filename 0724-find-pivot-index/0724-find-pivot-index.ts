0d17fb764bc15b81e4cda18b041d0fb5c556a74d
Sonky740 <mykiller4@naver.com>
2024-01-26 00:17:03 +0900

    Time: 76 ms (37.77%), Space: 52.5 MB (11.53%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.ts
@@ -0,0 +1,13 @@
+function pivotIndex(nums: number[]): number {
+  let totalSum = nums.reduce((acc, num) => acc + num, 0);
+  let leftSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (leftSum === totalSum - leftSum - nums[i]) {
+      return i;
+    }
+    leftSum += nums[i];
+  }
+
+  return -1;
+}
