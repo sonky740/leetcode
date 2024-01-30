d8b020fa500c1d8deb54edca272c500a8489cb36
Sonky740 <mykiller4@naver.com>
2024-01-31 00:21:51 +0900

    Time: 65 ms (94.66%), Space: 57.4 MB (6.53%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.ts
@@ -0,0 +1,12 @@
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if (p === null && q === null) return true;
+    if (p === null || q === null) return false;
+    if (p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
+
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+    if (root === null) return false;
+    if (isSameTree(root, subRoot)) return true;
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+}
