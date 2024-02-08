1c04acf833f98146364df347a1a98e1f4f477385
Sonky740 <mykiller4@naver.com>
2024-02-09 01:11:12 +0900

    Time: 102 ms (18.18%), Space: 59.3 MB (27.27%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.ts
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.ts
@@ -1,17 +1,14 @@
 function rowAndMaximumOnes(mat: number[][]): number[] {
   let max = 0;
   let maxIndex = 0;
+
   for (let i = 0; i < mat.length; i++) {
-    let count = 0;
-    for (let j = 0; j < mat[i].length; j++) {
-      if (mat[i][j] === 1) {
-        count++;
-      }
-    }
+    const count = mat[i].filter((v) => v === 1).length;
     if (count > max) {
       max = count;
       maxIndex = i;
     }
   }
+
   return [maxIndex, max];
 }
